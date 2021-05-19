54c91f2d81fa57bbd860bbcf4e7059b050fd8c38
Angélica Sofía Pagés Burgos <68571768+angelicapages@users.noreply.github.com>
2021-05-19 20:25:17 -0300

    ahora se ve el título y se hace el fetch de últimos lanzamientos y de opulares

--- src/components/Populares.js
+++ src/components/Populares.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
-import MovieCards from './MovieCards'
+import { url_populares } from '../variables/variables'
+import useFetch from '../hooks/useFetch'
+import Tarjetas from './Tarjetas'
 
-export class Populares extends Component {
-    render() {
-        return (
-            <div>
-                <h1><MovieCards /></h1>
-            </div>
-        )
-    }
+const Populares = () => {
+    const peliculasPopulares = useFetch(url_populares)
+    return (
+        <section>
+            <h1>Películas Populares</h1>
+            <Tarjetas peliculas={peliculasPopulares} />
+        </section>
+    )
 }
 
 export default Populares
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,4 @@
 import { useEffect, useState } from 'react'
-import { base_url, api_key, query } from '../variables/variables'
-
-
 
 export default function useFetch(id) {
     const [pelicula, setPelicula] = useState([])
