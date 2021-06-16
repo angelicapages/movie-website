2f205bb00404edaf6f04de4ae2d819f38e5db89a
Angélica Sofía Pagés Burgos <68571768+angelicapages@users.noreply.github.com>
2021-06-16 19:00:12 -0300

    Intentando leer los datos dentro del objeto

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,67 +1,73 @@
-import { useState } from 'react'
-import { url_carrusel } from '../variables/variables'
-import useFetch from '../hooks/useFetch'
-import { base_url_img } from '../variables/variables'
-import { Link } from 'react-router-dom'
-import { Button } from './style/ButtonStyle'
-import { CarruselSection, CarruselImg, ContenedorDeFlecha } from './style/CarruselStyle'
+import { url_carrusel } from "../variables/variables";
+import useFetch from "../hooks/useFetch";
+import { base_url_img } from "../variables/variables";
+import { Link } from "react-router-dom";
+import { Button } from "./style/ButtonStyle";
+import {
+    CarruselSection,
+    CarruselImg,
+    ContenedorDeFlecha,
+} from "./style/CarruselStyle";
 import { Titulo, Texto } from "./style/TextoStyle";
-import theme from './style/Theme'
-import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md';
-import { ContenedorCentrado, Overlay, ContenedorDeTexto } from './style/ContenedoresStyle'
+import theme from "./style/Theme";
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
+import {
+    ContenedorCentrado,
+    Overlay,
+    ContenedorDeTexto,
+} from "./style/ContenedoresStyle";
 
 const Carrusel = () => {
-    const peliculasCarrusel = useFetch(url_carrusel)
-    const [indexEnCarrusel, setIndexEnCarrusel] = useState(0)
-    let pelicula = peliculasCarrusel[indexEnCarrusel]
-    console.log(pelicula)
-    let listaMap = [1]
-
+    const peliculasCarrusel = useFetch(url_carrusel);
+    let peliculas = [peliculasCarrusel[0]]
+let pelicula = [peliculas['0']]
+console.log(peliculas)
     return (
-
         <CarruselSection>
-
-            {listaMap.map(pelicula => {
+            {/* {peliculasCarrusel.map((pelicula) => 
                
-               return(
-               <article key={pelicula.id}>
+            )} */}
+            <article key={peliculas.id}>
+                <CarruselImg url={`${base_url_img}${peliculas.poster_path}`}>
+                    <Overlay>
+                        <ContenedorDeFlecha>
+                            <MdKeyboardArrowLeft />
+                        </ContenedorDeFlecha>
+                        <ContenedorDeTexto>
+                            <ContenedorCentrado>
+                                <Titulo width="auto" align="center">
+                                    {peliculas.original_name}
+                                    {peliculas.original_title}
+                                </Titulo>
 
-                    <CarruselImg url={`${base_url_img}${pelicula.poster_path}`}>
-                        <Overlay>
-                            <ContenedorDeFlecha>
-                                <MdKeyboardArrowLeft />
-                            </ContenedorDeFlecha>
-                            <ContenedorDeTexto>
+                                <Texto align="center" width="400px">
+                                    {peliculas.overview}
+                                </Texto>
+
                                 <ContenedorCentrado>
-
-                                    <Titulo width="auto" align="center">{pelicula.original_name}{pelicula.original_title}</Titulo>
-
-                                    <Texto align="center" width="400px">{pelicula.overview}</Texto>
-
-                                    <ContenedorCentrado>
-
-                                        <Link to={`peliculaDetalle/${pelicula.id}`} style={{ textDecoration: 'none', color: 'white' }} >
-                                            <Button backgroundColor={theme.colors.background} color={theme.colors.text}>
-                                                Ver más
-                                            </Button>
-                                        </Link>
-                                    </ContenedorCentrado>
-
+                                    <Link
+                                        to={`peliculaDetalle/${peliculas.id}`}
+                                        style={{ textDecoration: "none", color: "white" }}
+                                    >
+                                        <Button
+                                            backgroundColor={theme.colors.background}
+                                            color={theme.colors.text}
+                                        >
+                                            Ver más
+                                        </Button>
+                                    </Link>
                                 </ContenedorCentrado>
-                            </ContenedorDeTexto>
+                            </ContenedorCentrado>
+                        </ContenedorDeTexto>
 
-                            <ContenedorDeFlecha>
-                                {/* <MdKeyboardArrowRight onClick={setactualEnCarrusel(actualEnCarrusel+1)}/> */}
-                            </ContenedorDeFlecha>
-                        </Overlay>
-                    </CarruselImg>
-
-                </article>
-
-
-            )})}
-        </CarruselSection >
+                        <ContenedorDeFlecha>
+                            {/* <MdKeyboardArrowRight onClick={setactualEnCarrusel(actualEnCarrusel+1)}/> */}
+                        </ContenedorDeFlecha>
+                    </Overlay>
+                </CarruselImg>
+            </article>
+        </CarruselSection>
     )
 }
 
-export default Carrusel
+export default Carrusel;
